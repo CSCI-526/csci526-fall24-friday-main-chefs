fd7bae14b0374404008bf34a8961c4aab1d4896a
Koi Chen <kevin953215@gmail.com>
2024-11-05 15:59:03 -0800

    update

--- Assets/Scripts/ScenesChange/EatBulletEnd.cs
+++ Assets/Scripts/ScenesChange/EatBulletEnd.cs
@@ -8,11 +8,14 @@ public class EatBulletEnd : MonoBehaviour
     public int surviveTime = 10;
     public TextMeshProUGUI countDownText;
     public GameManagerScript gameManager;
+    public GameObject targetObject;
+    HealthController hpScript;
     private float timeLeft;
     // Start is called before the first frame update
     void Start()
     {
         timeLeft = surviveTime;
+        hpScript = targetObject.GetComponent<HealthController>();
     }
 
     // Update is called once per frame
@@ -23,6 +26,12 @@ public class EatBulletEnd : MonoBehaviour
         if (timeLeft < 0)
         {
             gameManager.gameOver("Fitness master!\n Perfectly control your energy!");
+        } else if (hpScript.currentHealth >= hpScript.maxHealth)
+        {
+            gameManager.gameOver("Lose!");
+        } else if (hpScript.currentHealth <= 0)
+        {
+            gameManager.gameOver("Lose!");
         }
     }
 }
