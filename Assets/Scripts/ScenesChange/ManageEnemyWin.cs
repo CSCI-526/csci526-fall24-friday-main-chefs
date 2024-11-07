1fdaa6ee21bd4f57867699e81c78247554af1377
Koi Chen <kevin953215@gmail.com>
2024-11-07 02:54:17 -0800

    update

---
+++ Assets/Scripts/ScenesChange/ManageEnemyWin.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ManageEnemyWin : MonoBehaviour
+{
+    public int surviveTime = 10;
+    public TextMeshProUGUI countDownText;
+    public GameManagerScript gameManager;
+    public GameObject targetObject;
+    HealthController hpScript;
+    private float timeLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = surviveTime;
+        hpScript = targetObject.GetComponent<HealthController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        countDownText.text = Mathf.Round(timeLeft).ToString();
+        if (timeLeft < 0)
+        {
+            gameManager.gameOver("Fitness master!\n Perfectly control your energy!");
+        } else if (hpScript.currentHealth >= hpScript.maxHealth)
+        {
+            gameManager.gameOver("Lose!");
+        } else if (hpScript.currentHealth <= 0)
+        {
+            gameManager.gameOver("Lose!");
+        }
+    }
+}
