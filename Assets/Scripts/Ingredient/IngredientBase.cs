5842f96077c95ac6a92b62878868a4af065ad7cb
Koi Chen <kevin953215@gmail.com>
2024-11-22 00:08:21 -0800

    add IngredientBase

---
+++ Assets/Scripts/Ingredient/IngredientBase.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientBase : MonoBehaviour
+{
+    public enum FoodType { Regular, Mentos, Soda }; // Define different ingredient types
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
